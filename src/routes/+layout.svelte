ecd0e38ae7b9b7506e355c94f5fede775df7dd51
lionelritchie29 <lionelrtchieee@gmail.com>
2023-02-08 20:16:11 +0700

    feat: add layout and navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script>
   import '../styles/app.css'
+	import Navbar from '../widgets/Navbar.svelte';
+
+  const baseLayoutClass = 'max-w-7xl mx-auto px-8'
 </script>
 
-<div>Layout</div>
+<div class="root">
+  <Navbar {baseLayoutClass}/>  
 
-<slot></slot>
+  <section class={`${baseLayoutClass} pt-4`}>
+    <slot></slot>
+  </section>
+</div>
---
+++ src/widgets/Navbar.svelte
@@ -0,0 +1,49 @@
+<script lang='ts'>
+  export let baseLayoutClass: string = '';
+
+  const links = [
+    {
+      title: 'Dashboard'
+    },
+    {
+      title: 'Wallets'
+    },
+    {
+      title: 'Records'
+    },
+    {
+      title: 'Statistics'
+    }
+  ]
+</script>
+
+<nav class={`${baseLayoutClass} py-4 flex justify-between items-center`}>
+  <div class="flex">
+    <div class="font-semibold mr-8">Money Manager</div>
+
+    <ul class="flex space-x-5">
+      {#each links as link }
+        <li class="hover:font-medium cursor-pointer">{link.title}</li>
+      {/each}
+    </ul>
+  </div>
+
+  <div class="flex items-center">
+    <div class="mr-6 text-sm">
+      <button class="bg-blue-600 text-white rounded-full py-1 px-4">+ Record</button>
+    </div>
+
+    <div class="flex items-center text-sm cursor-pointer">
+      <div class="w-10 h-10 rounded-full border bg-gray-300"></div>
+      <div class="ml-2">
+        <span class="block font-semibold flex items-center">
+          <span>Lionel Ritchie</span>
+          <span class="block -mt-1 ml-1">&#8964;</span>
+        </span>
+        <span class="text-xs block text-gray-400">Member</span>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<div class="border-b border-gray-300"></div>
