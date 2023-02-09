f44db12838f88b1e73820655f9eac3c427ce1942
lionelritchie29 <lionelrtchieee@gmail.com>
2023-02-09 17:59:07 +0700

    feat: add profile dropdown

--- src/widgets/Navbar.svelte
+++ src/widgets/Navbar.svelte
@@ -1,22 +1,3 @@
-<script lang='ts'>
-  export let baseLayoutClass: string = '';
-
-  const links = [
-    {
-      title: 'Dashboard'
-    },
-    {
-      title: 'Wallets'
-    },
-    {
-      title: 'Records'
-    },
-    {
-      title: 'Statistics'
-    }
-  ]
-</script>
-
 <nav class={`${baseLayoutClass} py-4 flex justify-between items-center`}>
   <div class="flex">
     <div class="font-semibold mr-8">Money Manager</div>
@@ -33,7 +14,8 @@
       <button class="bg-blue-600 text-white rounded-full py-1 px-4">+ Record</button>
     </div>
 
-    <div class="flex items-center text-sm cursor-pointer">
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div on:click={toggleProfileDropdown} class="flex items-center text-sm cursor-pointer relative">
       <div class="w-10 h-10 rounded-full border bg-gray-300"></div>
       <div class="ml-2">
         <span class="block font-semibold flex items-center">
@@ -42,8 +24,58 @@
         </span>
         <span class="text-xs block text-gray-400">Member</span>
       </div>
+
+      {#if showProfileDropdown}  
+        <ul class="absolute min-w-[12rem] shadow top-12 left-0 bg-white rounded border">
+          {#each userLinks as link}
+            <li>
+              <button class="p-4 hover:bg-gray-100 w-full flex items-center">
+                <svelte:component class="mr-3" this={link.icon} />
+                {link.title}</button>
+            </li>
+          {/each}
+        </ul>
+      {/if}
     </div>
   </div>
 </nav>
 
 <div class="border-b border-gray-300"></div>
+
+<script lang='ts'>
+  import {AdjustmentsHorizontal, ArrowRightOnRectangle} from 'svelte-heros-v2'
+
+  export let baseLayoutClass: string = '';
+  let showProfileDropdown = false;
+
+  const links = [
+    {
+      title: 'Dashboard'
+    },
+    {
+      title: 'Wallets'
+    },
+    {
+      title: 'Records'
+    },
+    {
+      title: 'Statistics'
+    }
+  ]
+
+  const userLinks = [
+    {
+      title: 'Settings',
+      icon: AdjustmentsHorizontal
+    },
+    {
+      title: 'Log out',
+      icon: ArrowRightOnRectangle
+    }
+  ]
+
+  const toggleProfileDropdown = () => {
+    console.log('run')
+    showProfileDropdown = !showProfileDropdown
+  }
+</script>
