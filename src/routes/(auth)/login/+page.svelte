cecb4ffd92c718636cbc3f907e2e96908fbe3403
lionelritchie29 <lionelrtchieee@gmail.com>
2023-02-13 19:01:07 +0700

    feat: fix wrong auth route usage + change login layout

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -31,7 +31,7 @@
       uid: result.user.uid
     }
 
-    const response = await fetch('/auth/login', {method: 'POST', body: JSON.stringify(user)});
+    const response = await fetch('/login', {method: 'POST', body: JSON.stringify(user)});
     const {success}: {success: boolean} = await response.json();
 
     if (!success) {
@@ -43,16 +43,17 @@
   }
 </script>
 
-<section class="flex justify-center items-center min-h-screen">
-  <div class="flex shadow w-3/5 rounded-md">
-    <div class="p-6 w-1/2 bg-white border-left bg-blue-500 flex items-center justify-center">
+<section class="flex justify-center items-center min-h-screen bg-gray-100">
+  <div class="flex flex-col shadow w-4/5 md:w-3/5 lg:w-2/5 rounded-md">
+    <div class="p-6 bg-white border-left bg-blue-500 flex items-center text-lg justify-center rounded-t md:rounded-t-0 md:rounded-l">
       <div class="font-semibold mr-8 flex text-white"> <Wallet class="mr-2" /> Money Manager</div>
     </div>
-    <div class=" w-1/2 p-6 border rounded bg-white">
+    <div class="p-6 border rounded bg-white">
       <p>Welcome,</p>
       <p class='text-sm text-gray-400'>Please sign in first.</p>
       <!-- svelte-ignore a11y-mouse-events-have-key-events -->
-      <button 
+      <div class="flex">
+        <button 
         class="mt-4"
         on:click={signInWithGoogle} 
         on:mouseover={onGoogleSignInBtnHover} 
@@ -60,6 +61,7 @@
       >
         <img src={googleSignInBtnImg} alt="Google Sign In Button" />
       </button>
+      </div>
     </div>
   </div>
 </section>
