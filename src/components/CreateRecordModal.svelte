d397bb754b0ac3dac864c2f7e9211884271be3f4
lionelritchie29 <lionelrtchieee@gmail.com>
2023-02-18 14:23:13 +0700

    feat: add record form modal

---
+++ src/components/CreateRecordModal.svelte
@@ -0,0 +1,56 @@
+<script lang='ts'>
+	import Modal from "../widgets/Modal.svelte";
+</script>
+
+<Modal on:close>
+  <span slot="header">Add New Record</span>
+
+  <form method="post" action="?/create">
+    <div class="text-sm mt-3">
+      <label class="block" for="type">Type</label>
+      <select class="border px-2 p-2 w-full rounded mt-1" name="type">
+        <option value="expense">Expense</option>
+        <option value="Income">Income</option>
+      </select>
+    </div>
+
+    <div class="text-sm mt-3">
+      <label class="block" for="type">Account</label>
+      <select class="border px-2 p-2 w-full rounded mt-1" name="type">
+        <option value="expense">BCA Xpresi</option>
+        <option value="Income">Cash</option>
+      </select>
+    </div>
+
+    <div class="flex space-x-2 mt-3">
+      <div class="text-sm w-2/3">
+        <label class="block" for="number">Amount</label>
+        <input required class="border p-2 w-full rounded mt-1" type="number" name="amount" value="0" min="0">
+      </div>
+
+      <div class="text-sm w-1/3">
+        <label class="block" for="currency">Currency</label>
+        <select class="border px-2 p-2 w-full rounded mt-1" name="currency">
+          <option value="IDR">IDR</option>
+          <option value="USD">USD</option>
+        </select>
+      </div>
+    </div>
+    
+    <div class="flex space-x-2 mt-3">
+      <div class="text-sm w-full">
+        <label class="block" for="currency">Date</label>
+        <input required class="border p-2 w-full rounded mt-1" type="datetime-local" name="date">
+      </div>
+    </div>
+
+    <div class="flex space-x-2 mt-3">
+      <div class="text-sm w-full">
+        <label class="block" for="currency">Description</label>
+        <input required class="border p-2 w-full rounded mt-1" type="text" name="description">
+      </div>
+    </div>
+
+    <button disabled type="submit" class="btn btn-success mt-6">Save</button>
+  </form>
+</Modal>
--- src/widgets/Navbar.svelte
+++ src/widgets/Navbar.svelte
@@ -1,61 +1,13 @@
-<nav class={`${baseLayoutClass} py-4 flex justify-between items-center`}>
-  <div class="flex">
-    <div class="font-semibold mr-8 flex"> <Wallet class="mr-2" /> Money Manager</div>
-
-    <ul class="flex space-x-5">
-      {#each links as link }
-        <li class={`hover:text-blue-600 cursor-pointer ${isActiveLink($page.url.pathname, link) ? 'font-semibold' : ''}`}>
-          <a href={link.href}>{link.title}</a>
-        </li>
-      {/each}
-    </ul>
-  </div>
-
-  <div class="flex items-center">
-    <div class="mr-6 text-sm">
-      <button class="bg-blue-600 text-white rounded-full py-1 px-4">+ Record</button>
-    </div>
-
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div on:click={toggleProfileDropdown} class="flex items-center text-sm cursor-pointer relative">
-      {#if $currentUser.photoUrl}
-        <img class="w-10 h-10 rounded-full border" alt="User" src={$currentUser.photoUrl} />
-      {:else}
-        <div class="w-10 h-10 rounded-full border bg-gray-300" />
-      {/if}
-      <div class="ml-2">
-        <span class="block font-semibold flex items-center">
-          <span>{$currentUser.displayName}</span>
-          <span class="block -mt-1 ml-1">&#8964;</span>
-        </span>
-        <span class="text-xs block text-gray-400">Member</span>
-      </div>
-
-      {#if showProfileDropdown}  
-        <ul class="absolute min-w-[12rem] shadow top-12 left-0 bg-white rounded border">
-          {#each userLinks as link}
-            <li>
-              <button on:click={link.action} class="p-4 hover:bg-gray-100 w-full flex items-center">
-                <svelte:component class="mr-3" this={link.icon} />
-                {link.title}</button>
-            </li>
-          {/each}
-        </ul>
-      {/if}
-    </div>
-  </div>
-</nav>
-
-<div class="border-b border-gray-300"></div>
-
 <script lang='ts'>
   import {AdjustmentsHorizontal, ArrowRightOnRectangle, Wallet} from 'svelte-heros-v2'
   import {page} from '$app/stores'
 	import { goto } from '$app/navigation';
   import {currentUser} from "$lib/auth"
+	import CreateRecordModal from '../components/CreateRecordModal.svelte';
 
   export let baseLayoutClass: string = '';
   let showProfileDropdown = false;
+  let showCreateRecord = false;
 
   const isActiveLink = (pathname: string, link: {title: string, href: string}) => {
     if (link.href === '/' && pathname === '/') return true;
@@ -112,3 +64,55 @@
     showProfileDropdown = !showProfileDropdown
   }
 </script>
+
+<nav class={`${baseLayoutClass} py-4 flex justify-between items-center`}>
+  <div class="flex">
+    <div class="font-semibold mr-8 flex"> <Wallet class="mr-2" /> Money Manager</div>
+
+    <ul class="flex space-x-5">
+      {#each links as link }
+        <li class={`hover:text-blue-600 cursor-pointer ${isActiveLink($page.url.pathname, link) ? 'font-semibold' : ''}`}>
+          <a href={link.href}>{link.title}</a>
+        </li>
+      {/each}
+    </ul>
+  </div>
+
+  <div class="flex items-center">
+    <div class="mr-6 text-sm">
+      <button on:click={() => showCreateRecord = true} class="btn btn-primary py-1">+ Record</button>
+    </div>
+
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div on:click={toggleProfileDropdown} class="flex items-center text-sm cursor-pointer relative">
+      {#if $currentUser.photoUrl}
+        <img class="w-10 h-10 rounded-full border" alt="User" src={$currentUser.photoUrl} />
+      {:else}
+        <div class="w-10 h-10 rounded-full border bg-gray-300" />
+      {/if}
+      <div class="ml-2">
+        <span class="block font-semibold flex items-center">
+          <span>{$currentUser.displayName}</span>
+          <span class="block -mt-1 ml-1">&#8964;</span>
+        </span>
+        <span class="text-xs block text-gray-400">Member</span>
+      </div>
+
+      {#if showProfileDropdown}  
+        <ul class="absolute min-w-[12rem] shadow top-12 left-0 bg-white rounded border">
+          {#each userLinks as link}
+            <li>
+              <button on:click={link.action} class="p-4 hover:bg-gray-100 w-full flex items-center">
+                <svelte:component class="mr-3" this={link.icon} />
+                {link.title}</button>
+            </li>
+          {/each}
+        </ul>
+      {/if}
+    </div>
+  </div>
+</nav>
+
+<div class="border-b border-gray-300"></div>
+
+{#if showCreateRecord} <CreateRecordModal on:close={() => showCreateRecord = false} /> {/if}
