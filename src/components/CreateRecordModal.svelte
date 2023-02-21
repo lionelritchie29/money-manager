f8ef445cb492d29c3576c15f14f7c6dc6ca2cfe0
lionelritchie29 <lionelrtchieee@gmail.com>
2023-02-21 07:15:10 +0700

    feat: add form value vinding

--- src/components/CreateRecordModal.svelte
+++ src/components/CreateRecordModal.svelte
@@ -3,23 +3,34 @@
 	import Modal from "../widgets/Modal.svelte";
 
   export let userWallets: Wallet[] = [];
+
+  let type: string = 'Expense';
+  let walletId: string = userWallets[0].id;
+  let amount : number = 0;
+  let currency: string = 'IDR';
+  let date: Date = new Date();
+  let description: string = '';
+
+  const createRecord = async () => {
+    console.log({type, walletId, amount, currency, date, description})
+  }
 </script>
 
 <Modal on:close>
   <span slot="header">Add New Record</span>
 
-  <form>
+  <form on:submit={createRecord}>
     <div class="text-sm mt-3">
       <label class="block" for="type">Type</label>
-      <select class="border px-2 p-2 w-full rounded mt-1" name="type">
-        <option value="expense">Expense</option>
+      <select bind:value={type} class="border px-2 p-2 w-full rounded mt-1" name="type">
+        <option value="Expense">Expense</option>
         <option value="Income">Income</option>
       </select>
     </div>
 
     <div class="text-sm mt-3">
-      <label class="block" for="type">Account</label>
-      <select class="border px-2 p-2 w-full rounded mt-1" name="walletId">
+      <label class="block" for="type">Wallet</label>
+      <select bind:value={walletId} class="border px-2 p-2 w-full rounded mt-1" name="walletId">
         {#each userWallets as wallet}
           <option value={wallet.id}>{wallet.name}</option>
         {/each}
@@ -29,12 +40,12 @@
     <div class="flex space-x-2 mt-3">
       <div class="text-sm w-2/3">
         <label class="block" for="number">Amount</label>
-        <input required class="border p-2 w-full rounded mt-1" type="number" name="amount" value="0" min="0">
+        <input bind:value={amount} required class="border p-2 w-full rounded mt-1" type="number" name="amount" min="0">
       </div>
 
       <div class="text-sm w-1/3">
         <label class="block" for="currency">Currency</label>
-        <select class="border px-2 p-2 w-full rounded mt-1" name="currency">
+        <select bind:value={currency} class="border px-2 p-2 w-full rounded mt-1" name="currency">
           <option value="IDR">IDR</option>
           <option value="USD">USD</option>
         </select>
@@ -44,17 +55,17 @@
     <div class="flex space-x-2 mt-3">
       <div class="text-sm w-full">
         <label class="block" for="currency">Date</label>
-        <input required class="border p-2 w-full rounded mt-1" type="datetime-local" name="date">
+        <input bind:value={date} required class="border p-2 w-full rounded mt-1" type="datetime-local" name="date">
       </div>
     </div>
 
     <div class="flex space-x-2 mt-3">
       <div class="text-sm w-full">
         <label class="block" for="currency">Description</label>
-        <input required class="border p-2 w-full rounded mt-1" type="text" name="description">
+        <input bind:value={description} required class="border p-2 w-full rounded mt-1" type="text" name="description">
       </div>
     </div>
 
-    <button disabled type="submit" class="btn btn-success mt-6">Save</button>
+    <button type="submit" class="btn btn-success mt-6">Save</button>
   </form>
 </Modal>
--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -2,7 +2,7 @@ import type { ApiResponse } from '../../types/Response';
 import type { Wallet } from '../../types/Wallet';
 import type { LayoutLoad } from './$types';
 
-export const load: LayoutLoad = async ({ data }) => {
+export const load: LayoutLoad = async ({ data, fetch }) => {
 	const userWalletsFetchResponse = await fetch('/api/wallets');
 	const userWalletsResponse: ApiResponse<Wallet[]> = await userWalletsFetchResponse.json();
 	const user = data.user;
