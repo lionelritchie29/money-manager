24fde397a008607d5ace8ec05358029e4d273c08
lionelritchie29 <lionelrtchieee@gmail.com>
2023-02-22 17:57:37 +0700

    feat: add record modal in record page

--- src/routes/(app)/records/+page.svelte
+++ src/routes/(app)/records/+page.svelte
@@ -1,22 +1,24 @@
 <script lang='ts'>
+	import SideLayout from "../../../widgets/SideLayout.svelte";
+  import { showCreateRecordModal } from '$lib/utils/create-record';
+
   const records = [1,2,3,4,5,6]
 </script>
 
-<section class="flex space-x-4">
-  <div class="w-1/4 min-h-[50rem] p-6 bg-white shadow-sm rounded-lg">
-    <h1 class="text-2xl font-bold mb-2">Records</h1>
+<SideLayout>
+  <span slot="title">Records</span>
 
+  <div slot="side-content">
+    <button class="btn btn-success" on:click={() => $showCreateRecordModal = true}>+ Add Record</button>
   </div>
 
-  <div class="w-3/4">
-    <ul class="grid grid-cols-1 gap-4">
-      {#each records as record}
-        <li>
-          <div class="rounded-lg bg-white shadow-sm h-20">
+  <ul slot="main-content" class="grid grid-cols-1 gap-4">
+    {#each records as record}
+      <li>
+        <div class="rounded-lg bg-white shadow-sm h-20">
 
-          </div>
-        </li>
-      {/each}
-    </ul>
-  </div>
-</section>
+        </div>
+      </li>
+    {/each}
+  </ul>
+</SideLayout>
